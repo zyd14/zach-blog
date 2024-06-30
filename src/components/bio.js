ed57e49d82f7d5d2e9746c183ab16074f391b858
zyd14 <romerzs14@gmail.com>
2024-06-29 18:49:39 -0600

    Removing twitter link

--- src/components/bio.js
+++ src/components/bio.js
@@ -46,8 +46,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href={`https://github.com/zyd14`}>
+            His Github profile can be found here
           </a>
         </p>
       )}
